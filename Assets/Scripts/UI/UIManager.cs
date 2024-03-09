10799760422389e4b66b27975287c48c3248c8e5
Brekonat <98459040+Brekonat@users.noreply.github.com>
2024-03-09 11:49:21 +0000

    Completed Main UI
    + Audio Integration
    + Options Menu
    + Audio adjustment
    + Control Scheme Menu
    + Play Functionality
    + Quit Functionality

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,11 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    #region Variables
     [SerializeField] GameObject currentActiveObject;
+    [Header("Audio")]
+    [SerializeField] Slider musicSlider;
+    [SerializeField] Slider sfxSlider;
+    [SerializeField] Toggle musicToggle;
+    [SerializeField] Toggle sfxToggle;
 
+    // Events
+    public static event Action<float, float> OnAudioUpdatedEvent;
+    //public static void OnAudioUpdated(float musicVolume, float sfxVolume) { OnAudioUpdatedEvent?.Invoke(musicVolume, sfxVolume); }
+    #endregion
     public void ChangeObject(GameObject objectToActivate)
     {
         currentActiveObject.SetActive(false);
@@ -13,5 +26,46 @@ public class UIManager : MonoBehaviour
         currentActiveObject = objectToActivate;
 
         currentActiveObject.SetActive(true);
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private void Start()
+    {
+        UpdateAudio();
+    }
+
+    public void UpdateAudio()
+    {
+        float musicVolume;
+        float sfxVolume;
+
+        if(musicToggle.isOn)
+        {
+            musicVolume = musicSlider.value;
+        }
+        else
+        {
+            musicVolume = 0;
+        }
+
+        if(sfxToggle.isOn)
+        {
+            sfxVolume = sfxSlider.value;
+        }
+        else
+        {
+            sfxVolume = 0;
+        }
+
+        OnAudioUpdatedEvent?.Invoke(musicVolume, sfxVolume);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
