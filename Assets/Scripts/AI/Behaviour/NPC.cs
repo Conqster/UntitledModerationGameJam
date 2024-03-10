185314b3944fc7749b02f324b39a945997129906
Jagunmolu <okejagunmolu@gmail.com>
2024-03-10 13:19:23 +0000

    AI Module from Jaygun

---
+++ Assets/Scripts/AI/Behaviour/NPC.cs
@@ -0,0 +1,134 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEditor;
+
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class NPC : MonoBehaviour
+{
+    private StateMachine NPC_SM;
+    private uint id = 0;
+    private NavMeshAgent agent;
+
+    public uint Id
+    {
+        get { return id; }
+        set { id = value; }
+    }
+
+    [SerializeField] private SM_BrainInput m_brainInput;
+    [SerializeField] private SM_BrainOutput m_brainOutput;
+
+    [Space]
+    [Header("Dev Debugger")]
+    [SerializeField] private StateMachineData SMData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_brainInput.navAgent = GetComponent<NavMeshAgent>();
+        agent = GetComponent<NavMeshAgent>();
+        m_brainInput.npc = this;
+        m_brainInput.self = transform;
+        m_brainInput.myDrinkSlot.user = this;
+
+        m_brainInput.barEntranceLocation = GameObject.FindGameObjectWithTag("BarEntrance").transform;
+        m_brainInput.barCounterLocation = GameObject.FindGameObjectWithTag("BarCounter").transform;
+        m_brainInput.barDanceLocation = GameObject.FindGameObjectWithTag("BarDanceFloor").transform;
+
+        NPC_SM = new SM_Idle(m_brainInput, m_brainOutput);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        NPC_SM = NPC_SM.Process();
+        SMData = NPC_SM.GetStateMachineData();
+
+
+        //Test
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //    GrantAccessToBar();
+    }
+
+
+
+
+    /// <summary>
+    /// change NPC Bar Access state to Granted 
+    /// </summary>
+    public void GrantAccessToBar()
+    {
+        NPC_BarManager.Instance.RemoveFromQuene(this);
+        NPC_SM.ChangeBarAccessState(SM_BarAccessState.Granted);
+    }
+
+
+    /// <summary>
+    /// Decline NPC access
+    ///
+    /// </summary>
+    public void DeclineAccessToBar()
+    {
+        Destroy(this.gameObject);
+    }
+
+
+    public void UpdateRowdiness(float rate)
+    {
+        m_brainInput.rowdinessBehaviour = rate;
+    }
+
+    public void UpdateMoveTo(Vector3 pos)
+    {
+        if (agent.isStopped)
+            agent.isStopped = false;
+
+        agent.SetDestination(pos);
+    }
+
+    public void UpdateEntranceBasedOnQuene(Vector3 pos)
+    {
+        m_brainInput.barEntranceLocation.position = pos;
+        //Not Ideal, but it works
+        //UpdateMoveTo(pos);
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position + Vector3.up, 0.5f);
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, 0.5f);
+
+
+
+        //Debug Entrance Radius
+        if(m_brainInput != null)
+        {
+            if (!m_brainInput.barEntranceLocation && !m_brainInput.barDanceLocation && !m_brainInput.barCounterLocation)
+                return;
+
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(m_brainInput.barEntranceLocation.position, m_brainInput.reachedEntranceTreashold);
+
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(m_brainInput.barDanceLocation.position, m_brainInput.danceFloorTreshold);
+
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawWireSphere(m_brainInput.barCounterLocation.position, m_brainInput.reachedEntranceTreashold);
+
+
+
+            Handles.color = Color.red;
+            Handles.Label(transform.position + (4.0f * Vector3.up), m_brainInput.rowdinessBehaviour.ToString("0.0"));
+
+
+        }
+        
+    }
+}
--- Assets/Scripts/AI/StateMachine/StateMachine.cs
+++ Assets/Scripts/AI/StateMachine/StateMachine.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.AI;
 
 public enum SM_Event
 {
@@ -14,8 +15,38 @@ public enum SM_Event
 public enum SM_State
 {
     Idle,
-    MoveTo,
-    WaitInQuene
+    MoveToEntrance,
+    ApproachingDanceFloor,
+    ApproachingCounter
+    //MoveTo,
+    //WaitInQuene
+}
+
+public enum SM_BarAccessState
+{
+    Neutral,
+    WaitInQuene,
+    Granted,
+    Declined
+}
+
+public enum SM_GettingDrinkState
+{
+    //None,
+    Neutral, 
+    HaveASlot,
+    WaitingForDrink,
+    GotADrink,
+}
+
+public enum SM_DanceFloorState
+{
+    Neutral,
+    Approaching,
+    FindingASpot,
+    ApproachingSpot,
+    DancingInSpot
+    //I can have extra state, that could then extranally kick Npc out this state 
 }
 
 [System.Serializable]
@@ -25,6 +56,11 @@ public struct StateMachineData
     public SM_Event stateEvent;
     public float stateDuration;
     public SM_State state;
+
+    [Header("Mini-States")]
+    public SM_BarAccessState barAccessState;
+    public SM_GettingDrinkState gettingDrinkState;
+    public SM_DanceFloorState dancingState;
 }
 
 
@@ -39,13 +75,24 @@ public class StateMachine
     protected float sm_duration;
 
     protected SM_State sm_state;
+    protected SM_BarAccessState sm_barAccessState;
+    protected SM_GettingDrinkState sm_gettingDrinkState;
+    protected SM_DanceFloorState sm_dancingState;
+    protected SM_BrainInput sm_input;
+    protected SM_BrainOutput sm_output;
 
 
-    public StateMachine()
+    public StateMachine(SM_BrainInput input, SM_BrainOutput output)
     {
         sm_name = "Base State";
         sm_event = SM_Event.Enter;
         sm_duration = 0.0f;
+        sm_input = input;   
+        sm_output = output;  
+
+        sm_barAccessState = output.barAccessState;
+        sm_gettingDrinkState = output.gettingDrinkState;
+        sm_dancingState = output.danceFloorState;
     }
 
 
@@ -55,6 +102,9 @@ public class StateMachine
         sm_stateData.stateDuration = sm_duration;
         sm_stateData.stateEvent = sm_event;
         sm_stateData.state = sm_state;
+        sm_stateData.barAccessState = sm_barAccessState;
+        sm_stateData.gettingDrinkState = sm_gettingDrinkState;
+        sm_stateData.dancingState = sm_dancingState;
 
 
         switch (sm_event)
@@ -98,7 +148,6 @@ public class StateMachine
 
 
 
-
         if (sm_transitionTriggered)
             return;
 
@@ -127,8 +176,29 @@ public class StateMachine
     public string GetSM_Name() => sm_name;
     public SM_Event GetSM_Event() => sm_event;
 
+    public SM_BarAccessState GetSM_BarAccessState => sm_barAccessState;
+
+    public void ChangeBarAccessState(SM_BarAccessState state) 
+    { 
+        sm_output.barAccessState = state;
+        sm_barAccessState = state; 
+    }
+
+
+
+
     public StateMachineData GetStateMachineData() => sm_stateData;
 
     private StateMachine GetStateMachine() => this;
+
+
+    protected void MoveTo(Vector3 location)
+    {
+        //sm_input.navAgent.Move(location);
+        if(sm_input.navAgent.isStopped)
+            sm_input.navAgent.isStopped = false;
+
+        sm_input.navAgent.SetDestination(location);
+    }
 
 }
